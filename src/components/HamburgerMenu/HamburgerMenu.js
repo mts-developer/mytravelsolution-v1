60fdcadf2facc1c7443a9f9734c0d0efa58b252d
mts-developer <itmanagement@mytravelsolution.com.au>
2019-05-22 13:01:47 +1000

    Add hamburger menu

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -4,14 +4,41 @@ import "./hamburgermenu.css";
 class HamburgerMenu extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {open: false};
   }
 
-  render() {
+  toggleMenu = (e) => {
+    e.preventDefault()
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  toggleClass = () => {
     return (
-      <div>
-      <h1>Hello</h1>
+      this.state.open ? (
+        <div>
+          <div className={"hamburgermenu__bar hamburgermenu__bar--top--open"}/>
+          <div className={"hamburgermenu__bar hamburgermenu__bar--middle--open"}/>
+          <div className={"hamburgermenu__bar hamburgermenu__bar--bottom--open"}/>
+        </div>
+      ) : (
+        <div>
+          <div className={"hamburgermenu__bar hamburgermenu__bar--top"}/>
+          <div className={"hamburgermenu__bar"}/>
+          <div className={"hamburgermenu__bar hamburgermenu__bar--bottom"}/>
+        </div>
+      )
+    )
+  }
+
+  render(){
+    return(
+      <div onClick={e => this.toggleMenu(e)}>
+        {this.toggleClass()}
       </div>
-    );
+      
+    )
   }
 }
 
