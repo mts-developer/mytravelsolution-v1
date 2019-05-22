926d62d97e215132def34520f3e588a17acc2d9b
mts-developer <itmanagement@mytravelsolution.com.au>
2019-05-22 14:21:08 +1000

    WIP: Add Header Menu

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -4,41 +4,58 @@ import "./hamburgermenu.css";
 class HamburgerMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = { open: false };
   }
 
-  toggleMenu = (e) => {
-    e.preventDefault()
+  toggleClick = e => {
+    e.preventDefault();
     this.setState({
       open: !this.state.open
-    })
-  }
+    });
+  };
 
-  toggleClass = () => {
-    return (
-      this.state.open ? (
-        <div>
-          <div className={"hamburgermenu__bar hamburgermenu__bar--top--open"}/>
-          <div className={"hamburgermenu__bar hamburgermenu__bar--middle--open"}/>
-          <div className={"hamburgermenu__bar hamburgermenu__bar--bottom--open"}/>
-        </div>
-      ) : (
-        <div>
-          <div className={"hamburgermenu__bar hamburgermenu__bar--top"}/>
-          <div className={"hamburgermenu__bar"}/>
-          <div className={"hamburgermenu__bar hamburgermenu__bar--bottom"}/>
-        </div>
-      )
-    )
-  }
-
-  render(){
-    return(
-      <div onClick={e => this.toggleMenu(e)}>
-        {this.toggleClass()}
+  toggleHamburgerMenu = () => {
+    return this.state.open ? (
+      <div>
+        <div className={"hamburgermenu__bar hamburgermenu__bar--top--open"} />
+        <div
+          className={"hamburgermenu__bar hamburgermenu__bar--middle--open"}
+        />
+        <div
+          className={"hamburgermenu__bar hamburgermenu__bar--bottom--open"}
+        />
       </div>
-      
-    )
+    ) : (
+      <div>
+        <div className={"hamburgermenu__bar hamburgermenu__bar--top"} />
+        <div className={"hamburgermenu__bar"} />
+        <div className={"hamburgermenu__bar hamburgermenu__bar--bottom"} />
+      </div>
+    );
+  };
+
+  toggleMenu = () => {
+    return this.state.open ? (
+      <div
+        onClick={e => this.toggleClick(e)}
+        className="listmenu-container listmenu-container--open"
+      >
+        Hello
+      </div>
+    ) : (
+      <div className="listmenu-container">Hello</div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div onClick={e => this.toggleClick(e)}>
+          {this.toggleHamburgerMenu()}
+        </div>
+        {this.toggleMenu()}
+      </div>
+    );
   }
 }
 
