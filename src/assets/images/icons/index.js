3e15b6dbe0c6bc31186f34b326daab0900765318
mts-developer <itmanagement@mytravelsolution.com.au>
2019-05-29 13:03:58 +1000

    Fix transition delay by changing SVG to PNG for category icons

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -3,19 +3,11 @@
 import phone_white_svg from "./svg/mts-icon-white_phone.svg";
 import marker_white_svg from "./svg/mts-icon-white_marker.svg";
 import mail_white_svg from "./svg/mts-icon-white_mail.svg";
-import fb_white_svg from "./svg/mts_fb-white.svg";
+import fb_white_svg from "./svg/mts-icon-white_facebook.svg";
 
 import phone_grey_svg from "./svg/mts-icon-grey_phone.svg";
 import close_grey_svg from "./svg/mts-icon-grey_close.svg";
 import flight_grey_svg from "./svg/mts-category-grey_flight.svg";
-
-import flight_white_svg from "./svg/mts-category-white_flight.svg";
-import hotel_white_svg from "./svg/mts-category-white_hotel.svg";
-import car_white_svg from "./svg/mts-category-white_car.svg";
-import cruise_white_svg from "./svg/mts-category-white_cruise.svg";
-import packaged_deals_white_svg from "./svg/mts-category-white_package-deals.svg";
-import insurance_white_svg from "./svg/mts-category-white_insurance.svg";
-import railway_white_svg from "./svg/mts-category-white_railway.svg";
 
 // PNG
 
@@ -27,6 +19,14 @@ import packaged_deals_grey_png from "./png/mts-category-grey_package-deals.png";
 import insurance_grey_png from "./png/mts-category-grey_insurance.png";
 import railway_grey_png from "./png/mts-category-grey_railway.png";
 
+import flight_white_png from "./png/mts-category-white_flight.png";
+import hotel_white_png from "./png/mts-category-white_hotel.png";
+import car_white_png from "./png/mts-category-white_car.png";
+import cruise_white_png from "./png/mts-category-white_cruise.png";
+import packaged_deals_white_png from "./png/mts-category-white_package-deals.png";
+import insurance_white_png from "./png/mts-category-white_insurance.png";
+import railway_white_png from "./png/mts-category-white_railway.png";
+
 export default {
   phone_white_svg,
   marker_white_svg,
@@ -35,18 +35,18 @@ export default {
   phone_grey_svg,
   close_grey_svg,
   flight_grey_svg,
-  flight_white_svg,
-  hotel_white_svg,
-  car_white_svg,
-  cruise_white_svg,
-  packaged_deals_white_svg,
-  insurance_white_svg,
-  railway_white_svg,
   flight_grey_png,
   hotel_grey_png,
   car_grey_png,
   cruise_grey_png,
   packaged_deals_grey_png,
   insurance_grey_png,
-  railway_grey_png
+  railway_grey_png,
+  flight_white_png,
+  hotel_white_png,
+  car_white_png,
+  cruise_white_png,
+  packaged_deals_white_png,
+  insurance_white_png,
+  railway_white_png
 };
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,43 +3,43 @@ import icons from "../../assets/images/icons";
 const services = [
   {
     label: "Airfares & Featured Flights",
-    light: icons.flight_white_svg,
+    light: icons.flight_white_png,
     dark: icons.flight_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Hotels & Accommodation",
-    light: icons.hotel_white_svg,
+    light: icons.hotel_white_png,
     dark: icons.hotel_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Car Hire",
-    light: icons.car_white_svg,
+    light: icons.car_white_png,
     dark: icons.car_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Cruise & Cruise Packages",
-    light: icons.cruise_white_svg,
+    light: icons.cruise_white_png,
     dark: icons.cruise_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Packaged Deals",
-    light: icons.packaged_deals_white_svg,
+    light: icons.packaged_deals_white_png,
     dark: icons.packaged_deals_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Travel Insurance",
-    light: icons.insurance_white_svg,
+    light: icons.insurance_white_png,
     dark: icons.insurance_grey_png,
     url: "http://localhost:3000"
   },
   {
     label: "Rail Tickets",
-    light: icons.railway_white_svg,
+    light: icons.railway_white_png,
     dark: icons.railway_grey_png,
     url: "http://localhost:3000"
   }
