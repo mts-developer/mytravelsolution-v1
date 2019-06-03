ff10347586ea0f14bbc9634596b3a02e948a272d
mts-developer <itmanagement@mytravelsolution.com.au>
2019-06-03 12:54:08 +1000

    Revert footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
         />
       </div>
       <div className="center padding-20">
-        <p className="font--small white">© 2019 The Digital Butler</p>
+        <p className="font--small white">© 2019 MY TRAVEL SOLUTION</p>
       </div>
     </div>
   );
